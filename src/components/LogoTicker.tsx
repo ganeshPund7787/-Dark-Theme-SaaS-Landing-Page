425f33ebeeece1f5500bfe03780501d25a3ba589
ganeshPund7787 <ganupund7787@gmail.com>
2024-11-12 04:58:55 +0500

    add logo animation

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -7,6 +7,7 @@ import celestialLogo from "../assets/images/celestial.png";
 import pulseLogo from "../assets/images/pulse.png";
 import apexLogo from "../assets/images/apex.png";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 const images = [
   { src: acmeLogo, alt: "Acme Logo" },
@@ -24,14 +25,28 @@ export const LogoTicker = () => {
         <h2 className="text-xl text-center text-white/70">
           Trusted by the world's most innovative teams.
         </h2>
-        <div className="overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]">
-          <div className="flex gap-16">
+        <div className="overflow-hidden flex mt-9 before:content-[''] before:z-10 after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]">
+          <motion.div
+            transition={{
+              duration: 10,
+              ease: "linear",
+              repeat: Infinity,
+            }}
+            initial={{ translateX: 0 }}
+            animate={{ translateX: "-50%" }}
+            className="flex gap-16 flex-none pr-16"
+          >
             {images.map(({ src, alt }) => (
               <span key={Math.random()} className="">
                 <Image src={src} alt={alt} className="flex-none h-8 w-auto" />
               </span>
             ))}
-          </div>
+            {images.map(({ src, alt }) => (
+              <span key={Math.random()} className="">
+                <Image src={src} alt={alt} className="flex-none h-8 w-auto" />
+              </span>
+            ))}
+          </motion.div>
         </div>
       </div>
     </div>
